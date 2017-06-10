74540c81e92d9d64c68b5cccf8e824d33f047e4f
lei's PC <geraldleizhang@gmail.com>
2017-06-10 18:32:35 -0400

    adding parser rules

--- parser.go
+++ parser.go
@@ -9,25 +9,62 @@ import (
 
 type analysis struct {
 	XMLName           xml.Name          `xml:"analysis"`
-	Description       string            `xml:",innerxml"`
+	Cwsversion        string            `xml:"cwsversion,attr"`
+	Time              string            `xml:"time,attr"`
+	File              string            `xml:"file,attr"`
+	Md5               string            `xml:"md5,attr"`
+	Sha1              string            `xml:"sha1,attr"`
+	Logpath           string            `xml:"logpath,attr"`
 	Calltree          calltree          `xml:"calltree"`
 	Processes         processes         `xml:"processes"`
 	Running_processes running_processes `xml:"running_processes"`
 }
 
 type calltree struct {
-	Process_call string `xml:"process_call"`
-	Description  string `xml:",innerxml"`
+	Process_call_list []process_call `xml:"process_call"`
+}
+
+type process_call struct {
+	Index         string `xml:"index,attr"`
+	Pid           string `xml:"pid,attr"`
+	Filename      string `xml:"filename,attr"`
+	Filename_hash string `xml:"filename_hash,attr"`
+	Starttime     string `xml:"starttime,attr"`
+	Startreason   string `xml:"startreason,attr"`
+	calltree      string `xml:"calltree"`
 }
 
 type processes struct {
-	Process     string `xml:"process"`
-	Description string `xml:",innerxml"`
+	Process_list []process `xml:"process"`
+}
+
+type process struct {
+	Index             string `xml:"index,attr"`
+	Pid               string `xml:"pid,attr"`
+	Filename          string `xml:"filename,attr"`
+	Filename_hash     string `xml:"filename_hash,attr"`
+	Filesize          string `xml:"filesize,attr"`
+	Md5               string `xml:"md5,attr"`
+	Sha1              string `xml:"sha1,attr"`
+	Username          string `xml:"username,attr"`
+	Parentindex       string `xml:"parentindex,attr"`
+	Starttime         string `xml:"starttime,attr"`
+	Terminationtime   string `xml:"terminationtime,attr"`
+	Startreason       string `xml:"startreason,attr"`
+	Terminationreason string `xml:"terminationreason,attr"`
+	Executionstatus   string `xml:"executionstatus,attr"`
+	Applicationtype   string `xml:"applicationtype,attr"`
 }
 
 type running_processes struct {
-	Running_process string `xml:"running_process"`
-	Description     string `xml:",innerxml"`
+	Running_processes_list []running_process `xml:"running_process"`
+}
+
+type running_process struct {
+	Pid                string `xml:"pid,attr"`
+	Filename           string `xml:"filename,attr"`
+	Filename_hash      string `xml:"filename_hash,attr"`
+	Cmdline_parameters string `xml:"cmdline_parameters,attr"`
 }
 
 func main() {
@@ -54,5 +91,7 @@ func main() {
 
 	//fmt.Println(v)
 
-	fmt.Println(v.Calltree.Description)
+	for _, index := range v.Processes.Process_list {
+		fmt.Println(index.Pid)
+	}
 }
